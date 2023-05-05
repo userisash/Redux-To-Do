d3b990d90d4affa9382944f8403953260ff7d7f6
ashraf araj <ashrafthehero423@gmail.com>
2023-05-05 04:32:00 +0300

    new

---
+++ ToDo/src/App.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import 'bootstrap/dist/css/bootstrap.min.css';
+import AddTodoForm from './Components/AddTodoForm';
+import TodoList from './Components/TodoList';
+import TotalCompleteItems from './Components/TotalCompleteItems';
+
+const App = () => {
+	return (
+		<div className='container bg-white p-4 mt-5'>
+			<h1>My Todo List</h1>
+			<AddTodoForm />
+			<TodoList />
+			<TotalCompleteItems />
+		</div>
+	);
+};
+
+export default App;
--- ToDo/src/Components/AddToDoForm.jsx
+++ ToDo/src/Components/AddToDoForm.jsx
@@ -1,71 +1,38 @@
-import React,{useState, useEffect} from 'react'
+import React, { useState } from 'react';
 import { useDispatch } from 'react-redux';
-import { addTodo, handleEditSubmit } from '../Redux/ToDoApp/actions/Crud';
+import { addTodoAsync } from '../redux/todoSlice';
 
-export const Form = ({editFormVisibility, editTodo, cancelUpdate}) => {
+const AddTodoForm = () => {
+	const [value, setValue] = useState('');
+	const dispatch = useDispatch();
 
-  // dispatch function to dispatch an action
-  const dispatch = useDispatch();
+	const onSubmit = (event) => {
+		event.preventDefault();
+		if (value) {
+			dispatch(
+				addTodoAsync({
+					title: value,
+				})
+			);
+		}
+	};
 
-  // todo value state for normal add todo form
-  const [todoValue, setTodoValue]=useState('');
+	return (
+		<form onSubmit={onSubmit} className='form-inline mt-3 mb-3'>
+			<label className='sr-only'>Name</label>
+			<input
+				type='text'
+				className='form-control mb-2 mr-sm-2'
+				placeholder='Add todo...'
+				value={value}
+				onChange={(event) => setValue(event.target.value)}
+			></input>
 
-  // state for if someone changes the (to edit) value in update form
-  const [editValue, setEditValue]=useState('');
+			<button type='submit' className='btn btn-primary mb-2'>
+				Submit
+			</button>
+		</form>
+	);
+};
 
-  // useEffect is to show the (to edit) value in update form
-  useEffect(()=>{
-    setEditValue(editTodo.todo);
-  },[editTodo])
-
-  // normal add todo submit
-  const handleSubmit=(e)=>{
-      e.preventDefault();
-      let date = new Date();
-      let time = date.getTime();
-      let todoObj={
-          id: time,
-          todo: todoValue,
-          completed: false
-      }
-      setTodoValue('');
-      dispatch(addTodo(todoObj))
-  }
-
-  // update form submit
-  const editSubmit = (e) =>{
-    e.preventDefault();
-    let editedObj={
-      id: editTodo.id,
-      todo: editValue,
-      completed: false
-    }
-    dispatch(handleEditSubmit(editedObj))
-  }
-
-  return (
-    <>
-      {editFormVisibility===false?(
-        <form className='form-group custom-form' onSubmit={handleSubmit}>
-          <label>Add your todo-items</label>
-          <div className='input-and-btn'>
-              <input type="text" className='form-control' required
-              value={todoValue} onChange={(e)=>setTodoValue(e.target.value)}/>
-              <button type="submit" className='btn btn-secondary btn-md'>ADD</button>
-          </div>
-        </form>
-      ):(
-        <form className='form-group custom-form' onSubmit={editSubmit}>
-          <label>Update your todo-items</label>
-          <div className='input-and-btn'>
-              <input type="text" className='form-control' required
-              value={editValue||""} onChange={(e)=>setEditValue(e.target.value)}/>
-              <button type="submit" className='btn btn-secondary btn-md'>UPDATE</button>
-          </div>
-          <button type="button" className='btn btn-primary btn-md back-btn'
-          onClick={cancelUpdate}>BACK</button>
-        </form>
-      )}
-    </>
-  )
-}
+export default AddTodoForm;
