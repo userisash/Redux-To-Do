5eed6445e059927b5537aec8d3b4e0ecb71175db
ashraf araj <ashrafthehero423@gmail.com>
2023-05-05 04:13:41 +0300

    emptied files and created new once

--- ToDo/src/Redux/Store.jsx
+++
@@ -1,7 +0,0 @@
-import {configureStore} from 'redux';
-import { rootReducer } from './rootReducer';
-
- const store = configureStore(
-    rootReducer,
-)
-export default store
--- ToDo/src/Redux/rootReducer.jsx
+++
@@ -1,7 +0,0 @@
-import { combineReducers } from "redux";
-import { operationsReducer } from "./ToDoApp/reducers/operations";
-
-export const rootReducer = combineReducers({
-    operationsReducer,
-    // more reducers can be added here
-})
