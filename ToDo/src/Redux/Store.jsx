e79230c24956d2a9d36381e6c77f08005de63741
ashraf araj <ashrafthehero423@gmail.com>
2023-05-07 09:13:07 +0300

    fixed a typo in store.jsx

--- ToDo/src/Redux/Store.jsx
+++ ToDo/src/Redux/Store.jsx
@@ -1,8 +1,8 @@
 import { configureStore } from '@reduxjs/toolkit';
-import todoReducer from './todoSlice';
+import todoSlice from '../todoSlice.jsx';
 
 export default configureStore({
 	reducer: {
-		todos: todoReducer,
+		todos: todoSlice,
 	},
 });
